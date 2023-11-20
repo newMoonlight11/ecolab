01946e4444a0bdc06c8cf7a5048d5b7c855b37c9
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2023-11-19 23:04:18 -0500

    base de datos - modelos

--- ecolab/settings.py
+++ ecolab/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecolab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecolab',
+        'USER': 'Camila',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
